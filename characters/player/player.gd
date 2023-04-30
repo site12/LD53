d50891dbb4e2619d17e83540c276e851b988ae19
Shane <shanescott86@gmail.com>
2023-04-29 23:45:22 -0500

    omg so much shit has changed, idk man its more gamey

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -4,6 +4,8 @@ class_name Player extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var sussy = false
+
 @onready var animtree = $anims/AnimationTree
 
 var network_authority = false
@@ -12,20 +14,19 @@ enum PlayerState {
 	IDLE,
 	MOVING,
 	INTERACTING,
-	DEAD
-}
+	DEAD}
 
 @export var player_state = PlayerState.IDLE
 
+@onready var bodymeshes = [%head,%body]
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 
 #for multiplayer, allows us to figure out if the player is local or not
 func _enter_tree():
 	get_node("player_synchronizer").set_multiplayer_authority(str(name).to_int())
 	pass
-
 
 func _ready():
 	network_authority = get_node("player_synchronizer").is_multiplayer_authority()
@@ -33,12 +34,15 @@ func _ready():
 		%Camera3D.current = true
 	else:
 		%Camera3D.current = false
-	$UI/MarginContainer/Control.visible = %Camera3D.current
-	%id.text = str(GameInfo.peer_id)+ " "+GameInfo.get_player_name()
+	handle_name_label()
+	
+	if str(GameInfo.sroot.get_child(0).host_id) == name and %Camera3D.current:
+		GameInfo.sroot.get_child(0).hosting()
 
 func _process(_delta):
 	move_and_slide()
 	%camera_pos.position = position
+	%name_pos.position = position
 
 func _physics_process(delta):
 	handle_state()
@@ -47,11 +51,16 @@ func _physics_process(delta):
 		
 	handle_locations()
 	handle_movement(delta)
-	
+
+func handle_name_label():
+	var players = GameInfo.sroot.get_child(0).players_info
+	for player in players:
+		if str(player[0]) == str(name):
+			%name_label.text = player[1]
 
 func handle_locations():
 	#print($RayCast3D.get_collider().get_parent().name)
-	match $RayCast3D.get_collider().get_parent().name:
+	match $floorcast.get_collider().get_parent().name:
 		"church_main_building":
 			%location.text = "Church"
 		"hotel_main_bldg":
@@ -70,7 +79,6 @@ func handle_locations():
 			%location.text = "Store"
 		"MeshInstance3D":
 			%location.text = ""
-	
 
 func handle_movement(delta):
 	# Add the gravity.
@@ -101,3 +109,82 @@ func handle_state():
 	elif player_state == PlayerState.MOVING:
 		animtree["parameters/playback"].travel("jog")
 
+func set_skin(val):
+	match val:
+		1:
+			var skin = preload("res://characters/player/materials/skin1/skin1.tres")
+			var hat = preload("res://characters/player/materials/skin1/hat1.tres")
+			for x in bodymeshes:
+				x.material_override = skin
+			%cowboyhat.material_override = hat
+		2:
+			var skin = preload("res://characters/player/materials/skin2/skin2.tres")
+			var hat = preload("res://characters/player/materials/skin2/hat2.tres")
+			for x in bodymeshes:
+				x.material_override = skin
+			%cowboyhat.material_override = hat
+		3:
+			var skin = preload("res://characters/player/materials/skin3/skin3.tres")
+			var hat = preload("res://characters/player/materials/skin3/hat3.tres")
+			for x in bodymeshes:
+				x.material_override = skin
+			%cowboyhat.material_override = hat
+		4:
+			var skin = preload("res://characters/player/materials/skin4/skin4.tres")
+			var hat = preload("res://characters/player/materials/skin4/hat4.tres")
+			for x in bodymeshes:
+				x.material_override = skin
+			%cowboyhat.material_override = hat
+		5:
+			var skin = preload("res://characters/player/materials/skin5/skin5.tres")
+			var hat = preload("res://characters/player/materials/skin5/hat5.tres")
+			for x in bodymeshes:
+				x.material_override = skin
+			%cowboyhat.material_override = hat
+		6:
+			var skin = preload("res://characters/player/materials/skin6/skin6.tres")
+			var hat = preload("res://characters/player/materials/skin6/hat6.tres")
+			for x in bodymeshes:
+				x.material_override = skin
+			%cowboyhat.material_override = hat
+		7:
+			var skin = preload("res://characters/player/materials/skin7/skin7.tres")
+			var hat = preload("res://characters/player/materials/skin7/hat7.tres")
+			for x in bodymeshes:
+				x.material_override = skin
+			%cowboyhat.material_override = hat
+		8:
+			var skin = preload("res://characters/player/materials/skin8/skin8.tres")
+			var hat = preload("res://characters/player/materials/skin8/hat8.tres")
+			for x in bodymeshes:
+				x.material_override = skin
+			%cowboyhat.material_override = hat
+
+func highlight(val:bool):
+	var _highlight = preload("res://characters/player/materials/kill_highlight/kill_highlight.tres")
+	if val:
+		for x in bodymeshes:
+			x.material_overlay = _highlight
+			%cowboyhat.material_overlay = _highlight
+	else:
+		for x in bodymeshes:
+			x.material_overlay = null
+			%cowboyhat.material_overlay = null
+
+func _on_area_3d_area_entered(area):
+	var sus = GameInfo.sroot.get_child(0).sussy
+	#print(area.get_parent().name)
+	var baka = false
+	if area.get_parent().name == str(GameInfo.peer_id):
+		baka = true
+	if area.get_parent().is_in_group("player") and sus and not baka:
+		area.get_parent().highlight(true)
+
+func _on_area_3d_area_exited(area):
+	var sus = GameInfo.sroot.get_child(0).sussy
+	#print(area.get_parent().name)
+	var baka = false
+	if area.get_parent().name == str(GameInfo.peer_id):
+		baka = true
+	if area.get_parent().is_in_group("player") and sus and not baka:
+		area.get_parent().highlight(false)
--- networking/network_nodes/session/session.gd
+++ networking/network_nodes/session/session.gd
@@ -2,12 +2,13 @@ class_name Session extends Node
 
 @export var sname = ""
 @export var host_id = 0
-#player peer id, player name, spawned_state
+var sussy = false
+#player peer id, player name
 @export var players_info = []
 
 var player_scene = preload("res://characters/player/player.tscn")
 
-var players:Array[Player] = []
+var players:Array = []
 
 func _ready():
 	self.name = sname
@@ -25,11 +26,45 @@ func add_player_to_session(player_info:Array):
 	await get_tree().create_timer(1).timeout
 	server_spawn_player(player_info)
 	
-	
+@rpc("any_peer")
+func server_start_game():
+	start_game()
+	randomize()
+	var amogus = randi_range(0,players_info.size()-1)
+	#print(amogus)
+	var p = 0
+	for player in players_info:
+		rpc_id(player[0],"client_start_game")
+		if amogus == p:
+			rpc_id(player[0],"assign_role",true)
+		else:
+			rpc_id(player[0],"assign_role",false)
+		
+		p +=1
 
-	
-	
+@rpc
+func assign_role(is_sus):
+	if is_sus:
+		%monster.visible = true
+		%pre_game.visible = false
+		sussy = true
+	else:
+		%townsfolk.visible = true
+		%pre_game.visible = false
+	%AnimationPlayer.play("fade_assign")
 
+func hosting():
+	%host_controls.visible = true
+	%room_code.text = "Room Code: "+str(name)
+
+
+@rpc
+func client_start_game():
+	start_game()
+
+func start_game():
+	%blocker1.queue_free()
+	%blocker2.queue_free()
 
 func server_spawn_player(player_info):
 	spawn_player(player_info)
@@ -77,3 +112,15 @@ func remove_player_from_session(peer_id):
 			players.erase(player)
 			player.queue_free()
 
+
+
+func _on_help_open_button_up():
+	%help.visible = true
+
+
+func _on_help_close_button_up():
+	%help.visible = false
+
+
+func _on_begin_game_button_up():
+	rpc_id(1,"server_start_game")
